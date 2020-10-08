25fa98f5d50e8edadf71b1c28649b3cbb2bcf5d9
imhny <h-kishi2276@st.kyoto-art.ac.jp>
2020-10-08 14:41:18 +0900

    click

--- work/js/script.js
+++ work/js/script.js
@@ -1,7 +1,7 @@
 $(function(){
   
     //class="img_button"の要素をクリックした時、その要素の兄弟要素のclass="content"がスライドトグル表示/非表示される
-    $(".img_button").on("mouseover",function(){
+    $(".img_button").on("click",function(){
       let target = $(this).siblings(".content");
       target.slideToggle("slow");
   
