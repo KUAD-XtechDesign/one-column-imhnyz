c4ec11cd70f77b7f4cbbff9c3a54369cd165f68e
imhny <h-kishi2276@st.kyoto-art.ac.jp>
2020-10-08 17:46:24 +0900

    ki

--- work/js/script.js
+++ work/js/script.js
@@ -1,7 +1,5 @@
 $(function(){
-  
-    //class="img_button"の要素をクリックした時、その要素の兄弟要素のclass="content"がスライドトグル表示/非表示される
-    $(".img_button").on("click",function(){
+    $(".img_button").on("mouseover",function(){
       let target = $(this).siblings(".content");
       target.slideToggle("slow");
   
@@ -9,8 +7,8 @@ $(function(){
     })
   
     //class="close_button"の要素をクリックした時、その要素の親要素のclass="content"がスライドアップ非表示される
-    $(".close_button").on("click",function(){
-      $(this).parent(".content").slideUp("slow");
+    $(".close_button").on("mouseover",function(){
+      $(this).parent(".content").slidedown("slow");
     })
   
   });
